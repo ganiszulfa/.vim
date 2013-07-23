d77b13eedec31793047653b50b46f728b4c0b237
ganis <ganis@irvanda-OptiPlex-745.(none)>
2013-07-23 14:16:53 +0900

    adding the cloning scripts,, appearantly clones of git are not saved in the github

---
+++ bundle/cloning_scripts.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/bmcustodio/flake8
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/jcrocholl/pep8
+git clone https://github.com/vim-scripts/pydoc.vim
+git clone https://github.com/vim-scripts/pythoncomplete
+git clone https://github.com/ervandew/supertab
+git clone https://github.com/tomtom/tlib_vim
+git clone https://github.com/MarcWeber/vim-addon-mw-utils
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/garbas/vim-snipmate
+git clone https://github.com/honza/vim-snippets
