f61b63ae10c4018b6fce8213ecb99c6d0193763e
Ganis Z S <ganiszul.fa@gmail.com>
2015-11-27 15:50:52 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,13 +17,12 @@ echo "Installation is done."
 echo "-------------------------"
 echo " "
 
-echo "Changing terminal color, your previous setting is:"
-export $PS1
-# chaning command prompt color terminal
-PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ '
-alias ls="ls --color=auto"\
+echo "Changing terminal color, to do so:"
+# changing command prompt color terminal
+echo "PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ '"
+echo "alias ls=\"ls --color=auto\""
 # better dir display in ls\
-S_COLORS="di=31;1:ln=36;1:ex=31;1:*~=31;1:*.html=31;1:*.shtml=37;1"
+echo "S_COLORS=\"di=31;1:ln=36;1:ex=31;1:*~=31;1:*.html=31;1:*.shtml=37;1\"""
 
 
 echo " "
