5e8c591945f0e0b252ee09a7090cbbee79f8fcf6
Ganis Z S <ganiszul.fa@gmail.com>
2015-11-30 10:10:23 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -19,10 +19,11 @@ echo " "
 
 echo "Changing terminal color, to do so:"
 # changing command prompt color terminal
-echo "PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ '"
-echo "alias ls=\"ls --color=auto\""
+echo "PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+echo "alias ls=\"ls --color=auto\""  >> ~/.bashrc
 # better dir display in ls\
-echo "S_COLORS=\"di=31;1:ln=36;1:ex=31;1:*~=31;1:*.html=31;1:*.shtml=37;1\"""
+echo "S_COLORS=\"di=31;1:ln=36;1:ex=31;1:*~=31;1:*.html=31;1:*.shtml=37;1\"""  >> ~/.bashrc
+source ~/.bashrc
 
 
 echo " "
