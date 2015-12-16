0afdc433af3e14c5f8f00f3d09f2101c5f0cbb47
Ganis Z S <ganiszul.fa@gmail.com>
2015-12-16 14:48:20 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ echo "source ~/.vim/.vimrc" >> ~/.vimrc
 echo "done."
 echo " "
 echo "update or install vim"
-echo "sudo apt-get install vim"
+echo "sudo apt-get install -y vim"
 echo " "
 sudo apt-get install -y vim
 echo " "
@@ -17,15 +17,20 @@ echo "Installation is done."
 echo "-------------------------"
 echo " "
 
-echo "Changing terminal color, to do so:"
+echo "Changing terminal color..."
 # changing command prompt color terminal
 echo "PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;36m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+# better dir display in ls
 echo "alias ls=\"ls --color=auto\""  >> ~/.bashrc
-# better dir display in ls\
-echo "S_COLORS=\"di=31;1:ln=36;1:ex=31;1:*~=31;1:*.html=31;1:*.shtml=37;1\""  >> ~/.bashrc
 source ~/.bashrc
 
-
 echo " "
-echo "100%  done. Probably next is installing and configuring tmux."
+echo "VIM is finished. Next is tmux..."
+echo " "
+git clone https://github.com/ganiszulfa/.tmux.git ~/.tmux
+echo " "
+sh ~/.tmux/init.sh
+echo "Do this if u can't install tmux 1.9, but it takes a long time..."
+echo "curl -fsSL https://gist.githubusercontent.com/ganiszulfa/d6681695077e22a9fbf9/raw/9e565de715216ddc2fcfef1dc69763632cf3c262/install.sh | sudo bash -e"
+echo " "
 echo "-------------------------"
